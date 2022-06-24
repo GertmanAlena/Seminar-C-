8cd279b0fd43a7613b7eb0fe593a6119bad8a5c5
Alena <gertmanea@mail.ru>
2022-06-24 23:31:30 +0300

    первое задание

---
+++ Seminar_1/iteration_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа вывода квадрата числа
+
+//Запрашиваем у пользователя число
+Console.Write("Input number: ");
+
+//Создаём переменную number и сохраняем её значение, введённое из консоли
+int number = Convert.ToInt32(Console.ReadLine());
+
+// объявляем переменную q и сохраняем в неё квадрат числа number
+int q = (number * number);
+
+//выводим значение
+Console.WriteLine(number + "->" + q);
