384009dcb87eade405c4f6ff5b50070c41bda8ba
Alena <gertmanea@mail.ru>
2022-06-25 00:01:54 +0300

    iteration 2

---
+++ Seminar_1/iteration_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+Console.Write("Input numberA: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input numberB: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+    Console.Write("a =" + numberA + "b =" + numberB + "->"  + "->" + "Yes");
+else
+{
+    Console.Write("a =" + numberA + "b =" + numberB + "->"  + "->" + "No");
+}
