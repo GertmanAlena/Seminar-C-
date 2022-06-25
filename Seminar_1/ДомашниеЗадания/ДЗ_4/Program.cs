7f600f7454373d1f05dc6333260f6c0e8c90ecb3
Alena <gertmanea@mail.ru>
2022-06-25 22:36:47 +0300

    задача 8 чётные от 1 до N

--- Seminar_1/ДомашниеЗадания/ДЗ_4/Program.cs
+++ Seminar_1/ДомашниеЗадания/ДЗ_4/Program.cs
@@ -1,2 +1,25 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Input number ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int FirstNumber = 1;
+Console.Write(Number + "->");
+
+while(FirstNumber <= Number) 
+{
+    if(FirstNumber <= Number)
+        if(FirstNumber %2 ==0)
+            {
+                Console.Write(FirstNumber + ", ");
+                FirstNumber++;
+            }
+    else
+    {
+       FirstNumber++;
+    }
+ }
+
+
+
+
