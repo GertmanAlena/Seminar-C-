0b57b2f21109d734771e3f8892ddf69bf5195f03
Alena <gertmanea@mail.ru>
2022-06-25 17:50:17 +0300

    задача 2 д/з 1 +

--- Seminar_1/ДомашниеЗадания/ДЗ_1/Program.cs
+++ Seminar_1/ДомашниеЗадания/ДЗ_1/Program.cs
@@ -1,17 +1,22 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 Console.Write("Input number ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
+Double number_1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Input number ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
+Double number_2 = Convert.ToInt32(Console.ReadLine());
 
-int max = number_1;
+Double max = number_1;
 
 if (number_2 > max)
-Console.Write("a=" + number_1 + "; " + "b=" + number_2 + "; " + "-> " + " max=" + number_2);
-
-    else 
-    Console.Write("a=" + number_1 + "; " + "b=" + number_2 + "; " + "-> " + " max=" + max);
-
-        Console.Write("a=" + number_1 + "; " + "b=" + number_2 + "; " + "-> " + " числа равны");
+   {
+    Console.Write("a=" + number_1 + "; " + "b=" + number_2 + "; " + "-> " + " max=" + number_2);
+   }
+    else if (number_2 < max)
+        {
+            Console.Write("a=" + number_1 + "; " + "b=" + number_2 + "; " + "-> " + " max=" + max);
+        }
+else if (number_2 == max)
+{
+    Console.Write("a=" + number_1 + "; " + "b=" + number_2 + "; " + "-> " + " числа равны");
+}
