e279d82c0ccf1ea2fde1276167ce4587c90007af
Alena <gertmanea@mail.ru>
2022-06-25 15:07:27 +0300

    дни недели

--- Seminar_1/iteration_2/Program.cs
+++ Seminar_1/iteration_2/Program.cs
@@ -8,8 +8,11 @@ int number_1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number: ");
 int number_2 = Convert.ToInt32(Console.ReadLine());
 
+//добавили переменную q, можно без неё, тогда number_2 * number_2 нужно прописывать в if вместо q
+int q = number_2 * number_2;
+
 //сравниваем
-if (number_1 == number_2 * number_2)
+if (number_1 == q)
     Console.Write("a = " + number_1  + " b = " + number_2 + " -> " + "Yes");
 else
 {
---
+++ Seminar_1/iteration_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// выводим день недели по заданному числу
+
+Console.Write("Input day ");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+// Monday = 1; Tuesday = 2; Wednesday = 3; Thursday = 4; Friday = 5; Saturday = 6; Sunday = 7;
+
+if (day == 1)
+{
+    Console.Write("today is Monday");
+}
+
+else if (day == 2)
+{
+    Console.Write("today is Tuesday");
+}
+
+else if (day == 3)
+{
+    Console.Write("today is Wednesday");
+}
+
+else if (day == 4)
+{
+    Console.Write("today is Thursday");
+}
+
+else if (day ==5)
+{
+    Console.Write("today is Friday");
+}
+
+else if (day == 6)
+{
+    Console.Write("today is Saturday");
+}
+
+else if (day == 7)
+{
+    Console.Write("today is Sunday");
+}
+else Console.WriteLine("incorrect day");
