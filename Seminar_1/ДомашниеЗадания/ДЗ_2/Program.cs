1378c4018e728df4c9051ce2620907b63a54814f
Alena <gertmanea@mail.ru>
2022-06-25 19:07:43 +0300

    задача 4 д/з

--- Seminar_1/ДомашниеЗадания/ДЗ_2/Program.cs
+++ Seminar_1/ДомашниеЗадания/ДЗ_2/Program.cs
@@ -1,2 +1,23 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("Hello, World!");
+
+Console.Write("Input number ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if(number_2 > max) max = number_2;
+if(number_3 > max) max = number_3;
+Console.Write(number_1 + "," + number_2 + "," + number_3 + "," + "->");
+Console.Write(max);
+// else 
+// max = number_2;
+// Console.Write(number_1 + "," + number_2 + "," + number_3 + "," + "->");
+// Console.Write(max);
+
+
