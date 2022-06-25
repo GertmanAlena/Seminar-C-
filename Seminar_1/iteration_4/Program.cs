0c6d488720a7fd952e01cb17706fd6c6aa9cf43b
Alena <gertmanea@mail.ru>
2022-06-25 23:28:57 +0300

    задача на вывод от -N до N

---
+++ Seminar_1/iteration_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// // Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+
+Console.Write("Input number ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int FirstNumber = -Number;
+
+while(FirstNumber <= Number)
+{
+        if (FirstNumber <= Number)
+        Console.Write(FirstNumber + ", ");
+        FirstNumber++;
+}
