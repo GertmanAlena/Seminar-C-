e38ffbc2f8bc937133f4404d5edcd680470d8c3a
Alena <gertmanea@mail.ru>
2022-06-27 21:40:09 +0300

    123

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа выводит случацное число из отрезка [10,99] и показыввает большую цифру этого числа
+
+int number = new Random().Next(10, 100);
+
+int num1 = number / 10;
+int num2 = number % 10;
+
+if(num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
+Console.WriteLine(number +" " + num1 + " " + num2);
