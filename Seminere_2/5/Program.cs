f34475dc96e02a1078cb39e7bf9aba920c914f01
Alena <gertmanea@mail.ru>
2022-06-28 23:28:27 +0300

    пятая задача

---
+++ Seminere_2/5/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumTwoNumbers()
+{
+    Console.Write("Введите минимальное число диапазона ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное число диапазона ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    int sum = number1 + number2;
+    return sum;
+   
+}
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Сумма чисел: " + SumTwoNumbers());
+}
+
