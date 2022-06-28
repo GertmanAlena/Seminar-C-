759afd86554a557912e22770db56389a63502e39
Alena <gertmanea@mail.ru>
2022-06-28 23:17:45 +0300

    четвёртая задача

---
+++ Seminere_2/4/Program.cs
@@ -0,0 +1,18 @@
+﻿void rundomNumber(int min, int max){
+int number = new Random().Next(min, max+1);
+
+Console.Write(number + " -> ");
+if(number / 10 > number % 10)
+{
+    Console.WriteLine(number / 10);
+}
+else
+{
+    Console.WriteLine(number % 10);
+}
+}
+Console.Write("Введите минимальное число диапазона ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число диапазона ");
+int end = Convert.ToInt32(Console.ReadLine());
+rundomnumber(start, end);
