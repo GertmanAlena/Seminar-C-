fa9aeeabb6d24918a73ca9f03a50a13f113b2a6b
Alena <gertmanea@mail.ru>
2022-06-28 22:32:54 +0300

    третья задача

---
+++ Seminere_2/3/Program.cs
@@ -0,0 +1,17 @@
+﻿// тоже что и 2 задача, только с методом
+
+void rundomnumber(){
+int number = new Random().Next(10, 100);
+
+Console.Write(number + " -> ");
+if(number / 10 > number % 10)
+{
+    Console.WriteLine(number / 10);
+}
+else
+{
+    Console.WriteLine(number % 10);
+}
+}
+for (int i = 0; i < 5; i++)
+rundomnumber();
