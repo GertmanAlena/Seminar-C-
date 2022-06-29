72520580ff4fdc01dc40326613c9364f1404829e
Alena <gertmanea@mail.ru>
2022-06-29 22:47:03 +0300

    начала вторую задачу

--- Seminere_2/ДЗ/задача 2/Program.cs
+++ Seminere_2/ДЗ/задача 2/Program.cs
@@ -3,19 +3,16 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-void rundomNumber(int start, int finish)
+void FillArray ([] collection)
 {
-    int number = new Random().Next(start, finish+1);
-    int FerstNumber = number / 10;
-    int SecondNumber = FerstNumber % 10;
-    Console.Write(number + " -> " +  SecondNumber);
+    int length = collection.length;
+    int index = 0;
+    while(index<length){
+        collection[index] = new Random().Next();
+        index++;
+    }
 }
-0
-Console.Write("Введите минимальное число диапазона от 100 до 1000 ");
-int startNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите максимальное число диапазона от 100 до 1000 ");
-int endNumber = Convert.ToInt32(Console.ReadLine());
-
-rundomNumber(startNumber, endNumber);
-
-int rundomNumber()
+void PrintArrey(int[] collection)
+{
+    
+}
