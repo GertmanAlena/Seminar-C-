1090db242105899b3cf3907c37486b7e55122e40
Alena <gertmanea@mail.ru>
2022-06-29 22:34:35 +0300

    1 задача ДЗ2

--- Seminere_2/ДЗ/задача 1/Program.cs
+++ Seminere_2/ДЗ/задача 1/Program.cs
@@ -3,23 +3,6 @@
 // 782 -> 8
 // 918 -> 1
 
-// void rundomNumber(int start, int finish)
-// {
-//     int number = new Random().Next(start, finish+1);
-//     int FerstNumber = number / 10;
-//     int SecondNumber = FerstNumber % 10;
-//     Console.Write(number + " -> " +  SecondNumber);
-// }
-// 0
-// Console.Write("Введите минимальное число диапазона от 100 до 1000 ");
-// int startNumber = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите максимальное число диапазона от 100 до 1000 ");
-// int endNumber = Convert.ToInt32(Console.ReadLine());
-
-// rundomNumber(startNumber, endNumber);
-
-// int rundomNumber()
-
 int SNmetod ()
 {
    Console.Write("Введите трёхзначное число  ");
---
+++ Seminere_2/ДЗ/задача 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+void rundomNumber(int start, int finish)
+{
+    int number = new Random().Next(start, finish+1);
+    int FerstNumber = number / 10;
+    int SecondNumber = FerstNumber % 10;
+    Console.Write(number + " -> " +  SecondNumber);
+}
+0
+Console.Write("Введите минимальное число диапазона от 100 до 1000 ");
+int startNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число диапазона от 100 до 1000 ");
+int endNumber = Convert.ToInt32(Console.ReadLine());
+
+rundomNumber(startNumber, endNumber);
+
+int rundomNumber()
