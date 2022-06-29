9b32e5052511bfa46b5540ba7e2e90666dba8739
Alena <gertmanea@mail.ru>
2022-06-29 23:06:20 +0300

    начала вторую задачу0

--- Seminere_2/ДЗ/задача 2/Program.cs
+++ Seminere_2/ДЗ/задача 2/Program.cs
@@ -3,16 +3,55 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-void FillArray ([] collection)
+void FillArray(int[] collection)
 {
-    int length = collection.length;
+    int Length = collection.Length;
     int index = 0;
-    while(index<length){
-        collection[index] = new Random().Next();
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
-void PrintArrey(int[] collection)
+
+// метод void кторый будет печатать массив
+void PrintArray(int[] col)
 {
-    
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
 }
+
+// метод ind возвращает позицию index
+int IndexOf(int[] collection, int index)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == index)
+        {
+            position = index;
+            break;
+        }
+        index++;
+
+    }
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
