b08e55e45df04ae241b2f7b48ff959e75ca7813f
Alena <gertmanea@mail.ru>
2022-07-02 00:39:32 +0300

    мучила задачу

--- Seminere_2/ДЗ/задача 2/Program.cs
+++ Seminere_2/ДЗ/задача 2/Program.cs
@@ -2,39 +2,51 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
-void rundomNumber(int A, int B){
-int number = new Random().Next(A, B+1);
+// void rundomNumber(int A, int B){
+// int number = new Random().Next(A, B+1);
 
-// int number = new Random().Next(1, 100000);
-int min = 100;
+// // int number = new Random().Next(1, 100000);
+// int min = 100;
+// int max = 999;
+// int SecondMax = 9999;
+
+// if(number > min && number < max)
+// {
+//     int reduce = number % 10;
+//     Console.WriteLine(number + "->" + reduce);
+// }
+
+//     else if(number > max && number <= SecondMax)
+//     {
+//         int Div = number / 10;
+//         int Find = Div % 10;
+//         Console.Write(number + "->" + Find);
+//     }
+//             else if(number < min)
+//             {
+//                 Console.Write(number + "->" + "третьей цифры нет");
+//             }
+// else
+// {
+//     int bigNumber = number / 100;
+//     int Lot = bigNumber % 10;
+//     Console.WriteLine(number + "->" + Lot);
+// }
+// }
+// Console.Write("Введите минимальное число диапазона ");
+// int start = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное число диапазона до 99999 ");
+// int end = Convert.ToInt32(Console.ReadLine());
+// rundomNumber(start, end);
+
+
+int Number = new Random().Next(1, 100000);
+Console.Write(Number + "->");
 int max = 999;
-int SecondMax = 9999;
+int Div = 0;
+int Div2 = 0;
+int max2 = 99;
 
-if(number > min && number < max)
-{
-    int reduce = number % 10;
-    Console.WriteLine(number + "->" + reduce);
-}
 
-    else if(number > max && number <= SecondMax)
-    {
-        int Div = number / 10;
-        int Find = Div % 10;
-        Console.Write(number + "->" + Find);
-    }
-            else if(number < min)
-            {
-                Console.Write(number + "->" + "третьей цифры нет");
-            }
-else
-{
-    int bigNumber = number / 100;
-    int Lot = bigNumber % 10;
-    Console.WriteLine(number + "->" + Lot);
-}
-}
-Console.Write("Введите минимальное число диапазона ");
-int start = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите максимальное число диапазона до 99999 ");
-int end = Convert.ToInt32(Console.ReadLine());
-rundomNumber(start, end);
+for(Div = 999; Number > max; Number / 10);
+
