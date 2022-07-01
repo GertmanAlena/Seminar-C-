4e9ff58d8d63dc995cc255a54d7853efeef82e45
Alena <gertmanea@mail.ru>
2022-07-01 23:37:21 +0300

    Задаза 3 ДЗ_3

---
+++ Seminar_3/ДЗ_3/Задача_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9. 
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.WriteLine("Input number ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Number + "-> ");
+
+// if(Number >= 0)
+// {
+//     for(int current = 1; current < Number; current++)
+//     Console.Write(current * current * current + " ");
+// }
+// else
+// {
+//     for(int current = -1; current > Number; current--)
+//     Console.Write(current * current * current + " ");
+// }
+
+double Cube(int N){
+    return Math.Pow (N, 3);
+}
+Console.WriteLine("Input number ");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.Write(Number + "-> ");
+if(Number > 0)
+{
+    for(int current = 1; current < Number; current++)
+    Console.Write(Cube(current) + " ");
+}
+else
+{
+    for(int current = -1; current > Number; current--)
+    Console.Write(Cube(current) + " ");
+}
