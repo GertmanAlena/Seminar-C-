2ff17c41bb525a15dbe27d1ef912db5d2ed13002
Alena <gertmanea@mail.ru>
2022-07-01 22:26:27 +0300

    Задаза 3 ДЗ_3

---
+++ Seminar_3/ДЗ_3/Задача_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Double  SystemCoor(int Xa, int Ya, int Za, int Xb, int Yb, int Zb)
+{
+    return Math.Sqrt ((Xa - Xb) * (Xa - Xb) + (Ya - Yb) * (Ya - Yb) + (Za - Zb) * (Za - Zb));
+}
+
+    Console.WriteLine("введите координату Xa ");
+    int X1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите координату Ya ");
+    int Y1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите координату Za ");
+    int Z1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите координату Xb ");
+    int X2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите координату Yb ");
+    int Y2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите координату Zb ");
+    int Z2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(Math.Round(SystemCoor(X1, Y1, Z1, X2, Y2, Z2), 2));
+
+
+
