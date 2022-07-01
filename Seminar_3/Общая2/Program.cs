b426d3d8620da05efbacb20e5281cbdff589d3ad
Alena <gertmanea@mail.ru>
2022-07-01 19:51:43 +0300

    общая 2

--- Seminar_3/Общая2/Program.cs
+++ Seminar_3/Общая2/Program.cs
@@ -2,3 +2,7 @@
 
 Console.WriteLine("Input number ");
 int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(number + "-> ");
+
+for(int current =1; current <= number; current++)
+Console.Write(current*current + " ");
