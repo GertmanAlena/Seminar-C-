47cfd3fe57e5b423cbd68d4471172f0a473d5989
Alena <gertmanea@mail.ru>
2022-07-01 19:51:03 +0300

    начаала дз

---
+++ Seminar_3/ДЗ_3/Задача_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+void rundomNumber(int start){
+int RundomNumber = new Random().Next(10000, 100000);
+
+}
+
+int num5 = number % 10;
+int num4 = number / 10 % 10;
+int num3 = number / 100 % 10;
+int num2 = number / 1000 % 10;
+int num1 = number % 10000; 
