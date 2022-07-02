5b122e7ae0dfce2a119f07d93332cfd7bee2bb59
Alena <gertmanea@mail.ru>
2022-07-02 20:31:46 +0300

    метод 4

--- Lecture_3/Metod_4/Program.cs
+++ Lecture_3/Metod_4/Program.cs
@@ -1,14 +1,28 @@
-﻿// метод 4 принимает и возвращает Например нужно несколько раз написать текст
+﻿// // метод 4 принимает и возвращает Например нужно несколько раз написать текст
+
+// String Metod4(string text, int count)
+// {
+//     string result = string.Empty;
+//     int i = 0;
+//     while(i < count)
+//     {
+//     result = result + text;    
+//     i++;
+//     }
+//     return result;
+// }
+// String res = Metod4("Hello ", 3);
+// Console.WriteLine(res);
+
+// метод 4 принимает и возвращает Например нужно несколько раз написать текст FOR
 
 String Metod4(string text, int count)
 {
     string result = string.Empty;
-    int i = 0;
-    while(i < count)
+    for(int i = 0; i < count; i++)
     {
-    result = result + text;    
-    i++;
-    }
+        result = result + text;
+    }    
     return result;
 }
 String res = Metod4("Hello ", 3);
