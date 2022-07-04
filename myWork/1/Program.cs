f552b6c207c46edcb77985d3d3ae6a90a2da7ff9
Alena <gertmanea@mail.ru>
2022-07-04 18:19:44 +0300

    meters in kilometers

---
+++ myWork/1/Program.cs
@@ -0,0 +1,9 @@
+﻿// метры в километры
+
+Console.WriteLine("Input namber ");
+double meter = Convert.ToInt32(Console.ReadLine());
+double kilometer = 0;
+// if(meter < 10000)
+kilometer = meter / 1000;
+
+Console.Write(meter + "метров " + " = " + kilometer + " километров");
