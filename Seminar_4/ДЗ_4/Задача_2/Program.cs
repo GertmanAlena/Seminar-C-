1240880fb8b1ff304ee9bb021fdb8ae2015a9aff
Alena <gertmanea@mail.ru>
2022-07-05 20:00:41 +0300

    дз4 2

--- Seminar_4/ДЗ_4/Задача_2/Program.cs
+++ Seminar_4/ДЗ_4/Задача_2/Program.cs
@@ -2,3 +2,21 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int Summa(int num)
+{
+    int Sum = 0;
+    do
+    {
+        
+        int N1 = num % 10;
+        Sum = Sum + N1;
+        num = num / 10;
+    } while (num != 0);
+
+
+    return Sum;
+}
+Console.WriteLine("input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Summa(number));
