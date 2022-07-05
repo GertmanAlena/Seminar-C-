d1bd2b38c236212bad43fba4213962b1fd27eadb
Alena <gertmanea@mail.ru>
2022-07-05 22:17:30 +0300

    дз4 3

--- Seminar_4/ДЗ_4/Задача_3/Program.cs
+++ Seminar_4/ДЗ_4/Задача_3/Program.cs
@@ -1,3 +1,33 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+int[] CRRandonArrey (int volum, int min, int max)
+{
+    int[] RandomArray = new int [volum];
+    for (int i = 0; i < volum; i++)
+    {
+        RandomArray[i] = new Random().Next(min, max+1);
+    }
+    return RandomArray;
+}
+void PrintArrey (int[] arrey)
+{
+    int count = arrey.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(arrey[i] + ", ");
+    }
+}
+
+Console.Clear();
+
+Console.WriteLine("количество элементов");
+int lang = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите MIN");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите MAX");
+int end = Convert.ToInt32(Console.ReadLine());
+int[] myArrey = CRRandonArrey(lang, start, end);
+
+PrintArrey(myArrey);
