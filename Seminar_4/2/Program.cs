d946d157849734abf66da85822132bd0094e77a8
Alena <gertmanea@mail.ru>
2022-07-07 19:15:42 +0300

    2

---
+++ Seminar_4/2/Program.cs
@@ -0,0 +1,57 @@
+﻿// // сумма чисел
+
+// int Sum (int Number)
+// {
+//     int Summa = 0;
+//     for (int i = 0; i <= Number; i++)
+//     {
+//         Summa = Summa + i;
+//     }
+//     return Summa;
+// }
+
+// Console.WriteLine("N" );
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Sum(N));
+                            //********************************
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// int[] Arrey(int size, int start, int end)
+// {
+//     int[] myArrey = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         myArrey[i] = new Random().Next(start, end);
+//     }
+// return myArrey;
+// }
+// void PrintArrey (int[] ArreyPr)
+// {
+//     int count = ArreyPr.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write(ArreyPr[i] + ", ");
+//     }
+// }
+
+// Console.WriteLine("введите size ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите start ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите end ");
+// int mux = Convert.ToInt32(Console.ReadLine());
+// int[] RandomArrey = Arrey(length, min, mux);
+
+// PrintArrey(RandomArrey);
+                                    //-***-
+                                    //таблица умножения
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+// Console.WriteLine();
+// }
+
+
+
