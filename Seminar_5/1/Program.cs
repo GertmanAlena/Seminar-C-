a7e03c1ed568427eccfaee8e20854ce9a3c7ee73
Alena <gertmanea@mail.ru>
2022-07-07 23:20:32 +0300

    1

--- Seminar_5/1/Program.cs
+++ Seminar_5/1/Program.cs
@@ -40,35 +40,6 @@
                              //* * * * * 
 // Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2] 
-int[] CreateRandomArray(int size, int min, int max)
-{
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(min, max + 1);
-        Console.Write($"{array[i]}, ");
-    }
-    return array;
-}
-
-void ShowArray(int[] array)
-{
-     Console.WriteLine("-> [ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i] *-1}, ");
-    }
-    Console.Write(" ]");
-}
-int[] myArray = CreateRandomArray(12, -9, 9);
-ShowArray(myArray);
-
-                        // *******
-//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
-// 4; массив [6, 7, 19, 345, 3] -> нет
-// -3; массив [6, 7, 19, 345, 3] -> да
-
-
 
 // int[] CreateRandomArray(int size, int min, int max)
 // {
@@ -76,26 +47,70 @@ ShowArray(myArray);
 //     for (int i = 0; i < size; i++)
 //     {
 //         array[i] = new Random().Next(min, max + 1);
+//         Console.Write($"{array[i]}, ");
 //     }
 //     return array;
 // }
 
 // void ShowArray(int[] array)
 // {
-//     int find = 3;
+//      Console.WriteLine("-> [ ");
 //     for (int i = 0; i < array.Length; i++)
 //     {
-//         if (array[i] == find)
-//         {
-//             Console.Write(array[i] + "yes");
-//         }
-//         Console.Write(array[i] + " ");
+//         Console.Write($"{array[i] *-1}, ");
 //     }
-//     Console.WriteLine();
+//     Console.Write(" ]");
 // }
-
 // int[] myArray = CreateRandomArray(12, -9, 9);
 // ShowArray(myArray);
+
+                        // *******
+//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+        {
+        Console.Write(array[i] + " ");
+        }
+    Console.Write("]");
+    
+Console.WriteLine();
+}
+
+int FindArrey (int[] arrey)
+{
+    Console.WriteLine("Input FIND ");
+    int find = Convert.ToInt32(Console.ReadLine());
+    string msg = "No";
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        if (arrey[i] == find | arrey[i] == -find)
+        {
+            msg = "Yes";
+            break;
+        }
+    }
+    return msg;
+}
+int[] myArray = CreateRandomArray(12, -9, 9);
+
+FindArrey(myArray);
+ShowArray(myArray);
+
 
 
 
