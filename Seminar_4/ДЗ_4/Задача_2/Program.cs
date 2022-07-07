6393f36771863fb6b236b5ab73a8be8cd80a3516
Alena <gertmanea@mail.ru>
2022-07-07 20:48:06 +0300

    4

--- Seminar_4/ДЗ_4/Задача_2/Program.cs
+++ Seminar_4/ДЗ_4/Задача_2/Program.cs
@@ -7,14 +7,11 @@ int Summa(int num)
 {
     int Sum = 0;
     do
-    {
-        
+    {        
         int N1 = num % 10;
         Sum = Sum + N1;
         num = num / 10;
     } while (num != 0);
-
-
     return Sum;
 }
 Console.WriteLine("input number ");
