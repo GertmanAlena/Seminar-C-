2ac128cddcb8a4d8fa6f293c062b9320fdb10315
Alena <gertmanea@mail.ru>
2022-07-08 22:00:55 +0300

    дз 5 задача 3

---
+++ Seminar_5/ДЗ_5/Задача_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+double[] NewArrey(int size, int min, int max)
+{
+    Random rnd = new Random();
+    double[]  arrey = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+         arrey[i] = rnd.NextDouble()*(min-max)+min;
+    }
+   return arrey;
+}
+double[] myMass = NewArrey(10, 1, 5);
+void ShowArray(double[] mass)
+{
+    double max = mass[0];
+    Console.Write("[");
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] > max)
+        {
+            max = mass[i];
+        }
+        Console.Write($"{Math.Round(mass[i], 2)}, ");
+    }
+    Console.Write($"] -> {Math.Round(max, 2)}");
+}
+ShowArray(myMass);
