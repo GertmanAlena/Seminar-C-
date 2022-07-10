d7494361cdc08e5a6faabaa8857e7104d363cb44
Alena <gertmanea@mail.ru>
2022-07-10 19:18:30 +0300

    дз 5 задача 3

--- Seminar_5/ДЗ_5/Задача_3/Program.cs
+++ Seminar_5/ДЗ_5/Задача_3/Program.cs
@@ -17,6 +17,8 @@ double[] myMass = NewArrey(10, 1, 5);
 void ShowArray(double[] mass)
 {
     double max = mass[0];
+    double min = mass[0];
+    double difference = max - min;
     Console.Write("[");
     for (int i = 0; i < mass.Length; i++)
     {
@@ -24,8 +26,18 @@ void ShowArray(double[] mass)
         {
             max = mass[i];
         }
+        // Console.Write($"{Math.Round(mass[i], 2)}, ");
+        if (mass[i] < min)
+        {
+            min = mass[i];
+        }
         Console.Write($"{Math.Round(mass[i], 2)}, ");
+        
     }
-    Console.Write($"] -> {Math.Round(max, 2)}");
+    Console.Write("]");
+    Console.WriteLine();
+    Console.WriteLine($"-> максимальное число {Math.Round(max, 2)}");
+    Console.WriteLine($"-> минимальное число {Math.Round(min, 2)}");
+    Console.WriteLine($"-> разница {Math.Round(max - min, 2)}");
 }
 ShowArray(myMass);
