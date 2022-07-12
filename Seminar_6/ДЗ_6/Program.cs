ada5571a18f32a579a8546668b999798123746a8
Alena <gertmanea@mail.ru>
2022-07-12 22:31:36 +0300

    6

--- Seminar_6/ДЗ_6/Program.cs
+++ Seminar_6/ДЗ_6/Program.cs
@@ -3,40 +3,48 @@
 // 0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
-int[] myArrey()
+internal class Program
 {
-    Console.Write("Введите количество элементов массива:\t ");
-    int alementsCount = Convert.ToInt32(Console.ReadLine());
-    int[] arrey = new int [alementsCount];
-
-    for (int i = 0; i < arrey.Length; i++)
+    private static void Main(string[] args)
     {
-        Console.Write($"Введите элемент массива под индексом {i}:\t ");
-        arrey[i] = Convert.ToInt32(Console.ReadLine());
-    }
-    return arrey;
-}
-void PrintMass(int[] inArray){
-   
-    for (int i = 0; i < inArray.Length; i++)
-    {
-       Console.Write($"{inArray[i]} ");
-    }
-}
-
-int Cou(int[] mass){
-    int counts = 0;
-    for (int i = 0; i < mass.Length; i++)
-    {
-        if (mass[i] > 0)
+        int[] myArrey()
         {
-            counts += 1;
+            Console.Write("Введите количество элементов массива:\t ");
+            int alementsCount = Convert.ToInt32(Console.ReadLine());
+            int[] arrey = new int[alementsCount];
+
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}:\t ");
+                arrey[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return arrey;
         }
+        void PrintMass(int[] inArray)
+        {
+
+            for (int i = 0; i < inArray.Length; i++)
+            {
+                Console.Write($"{inArray[i]} ");
+            }
+        }
+
+        int Cou(int[] mass)
+        {
+            int counts = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] > 0)
+                {
+                    counts += 1;
+                }
+            }
+            Console.Write($" -> {counts} ");
+            return counts;
+
+        }
+        int[] array = myArrey();
+        PrintMass(array);
+        Cou(array);
     }
-    Console.Write($" -> {counts} ");
-    return counts;
-    
 }
-int[] array = myArrey();
-PrintMass(array);
-Cou(array);
