26e9fb78180663ec3622cfd2fa59478d4c8c3977
Alena <gertmanea@mail.ru>
2022-07-12 20:30:37 +0300

    дз 6

---
+++ Seminar_6/ДЗ_6/Program.cs
@@ -0,0 +1,42 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int[] myArrey()
+{
+    Console.Write("Введите количество элементов массива:\t ");
+    int alementsCount = Convert.ToInt32(Console.ReadLine());
+    int[] arrey = new int [alementsCount];
+
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t ");
+        arrey[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arrey;
+}
+void PrintMass(int[] inArray){
+   
+    for (int i = 0; i < inArray.Length; i++)
+    {
+       Console.Write($"{inArray[i]} ");
+    }
+}
+
+int Cou(int[] mass){
+    int counts = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] > 0)
+        {
+            counts += 1;
+        }
+    }
+    Console.Write($" -> {counts} ");
+    return counts;
+    
+}
+int[] array = myArrey();
+PrintMass(array);
+Cou(array);
