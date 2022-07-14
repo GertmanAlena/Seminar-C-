f611d7a20b40e8a3433a6dcae24f3b844fc1799a
Alena <gertmanea@mail.ru>
2022-07-14 20:03:50 +0300

    .\work6\

--- Seminar_6/work6/Program.cs
+++ Seminar_6/work6/Program.cs
@@ -3,19 +3,73 @@
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
 
-void Febonachi(){
+// void Febonachi(){
 
-    int amountNumberts = 12;
-    int number1 = 0;
-    int number2 = 1;
-    Console.Write($"{number1}, {number2}, ->");
-    for (int i = 0; i < amountNumberts; i++)
+//     int amountNumberts = 12;
+//     int number1 = 0;
+//     int number2 = 1;
+//     Console.Write($"{number1}, {number2}, ->");
+//     for (int i = 0; i < amountNumberts; i++)
+//     {
+
+//         int NextNumber = number1 + number2;
+//         number1 = number2;
+//         number2 = NextNumber;
+//         Console.Write($" {NextNumber}, ");
+//     }
+// }
+// Febonachi();
+
+// ***   ***   ***
+// Напишите программу, которая будет создавать копию 
+// заданного массива с помощью поэлементного копирования.
+
+
+    // int[] myArray1 = new int[5];
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     myArray1[i] = new Random().Next(1, 100);
+
+    //     int[] myArray2 = new int[5];
+    //     Console.Write(myArray1[i]);
+
+    //     for (int j = 0; j < myArray1.Length; j++)
+    //     {
+    //         myArray2[i] = myArray1[i];
+    //         Console.Write(myArray2[i]);
+    //     }
+       
+    // }
+
+int[] array1(){
+
+    int[] myArray1 = new int[5];
+    for (int i = 0; i < 5; i++)
     {
-        
-        int NextNumber = number1 + number2;
-        number1 = number2;
-        number2 = NextNumber;
-        Console.Write($" {NextNumber}, ");
+        myArray1[i] = new Random().Next(1, 100);
+    }
+    return myArray1;
+}
+
+void ShowArray1(int[] massive){
+    for (int i = 0; i < massive.Length; i++)
+    {
+        Console.Write($"{massive[i]}, ");
     }
 }
-Febonachi();
+
+int[] array2(int[] massive2){
+
+    int[] massive2 = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+       massive2[i] = myArray1[i];
+    }
+    return massive2;
+}
+int[] myMass1 = array1();
+ShowArray1(myMass1);
+array2(myMass1);
+
+
