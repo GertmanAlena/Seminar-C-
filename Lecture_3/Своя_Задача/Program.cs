f4a657d9fe81c1980ac30516fea6756a5b31fe6a
Alena <gertmanea@mail.ru>
2022-07-16 23:31:57 +0300

    ...

---
+++ Lecture_3/Своя_Задача/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+string text = "Изучаем массивы и функции, ";
+string Replace (string text, char oldSimbol, char newSimbol, char oldSimbol2, char newSimbol2)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for(int i = 0; i < length; i++) 
+        if(text[i] == oldSimbol) result = result + $"{newSimbol}";
+        else if(text[i] == oldSimbol2) result = result + $"{newSimbol2}";
+            else
+            result = result + $"{text[i]}";
+    return result;
+}
+string NewText = Replace(text, oldSimbol: 'а', newSimbol: 'А', oldSimbol2: 'у', newSimbol2: 'У');
+Console.WriteLine(NewText);
