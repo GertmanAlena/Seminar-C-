e53d5aa219eee405ef5b00a0c6aa0ad3fad4c2d4
Alena <gertmanea@mail.ru>
2022-07-17 21:41:55 +0300

    задача транспорт

---
+++ myWork/Mass_in_Mass/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задан прямоугольный массив названий и обозначений транспортных средств,
+// (a – автомобиль, v – велосипед, m - мотоцикл, p – поезд, s – самолет).
+// По введенному обозначению транспортного средства выводится его название
+
+string a = "avtomobile";
+string v = "velo";
+string m = "moto";
+string p = "train";
+string s = "plane";
+string[] avto = new string[] {a, v, m, p, s};
+
+Console.WriteLine("введите букву транспорта ");
+string? arg = Console.ReadLine();
+
+if (arg == "a") Console.Write($"a -> {a} \t");
+else if (arg == "v") Console.Write($"v -> {v} \t");
+else if (arg == "m") Console.Write($"m -> {m} \t");
+else if (arg == "p") Console.Write($"p -> {p} \t");
+else if (arg == "s") Console.Write($"s -> {s} \t");
+   
+
+else Console.WriteLine($"{arg} -> такого транспорта нет \t");
