2f4d8dc3031504ca39e43cc998f6adcabad87d78
Alena <gertmanea@mail.ru>
2022-07-22 00:13:22 +0300

    exe+

--- Seminar_9/work_9/Program.cs
+++ Seminar_9/work_9/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задайте значение N. Вывести все натуральные числа от 1 до N
-Console.Clear();
-Console.Write("Введите значение N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-// Numbers(N);
-Console.WriteLine(RecursiveNumbers(1, N));
+// Console.Clear();
+// Console.Write("Введите значение N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// // Numbers(N);
+// Console.WriteLine(RecursiveNumbers(1, N));
 
 // void Numbers(int num)
 // {
@@ -22,11 +22,43 @@ Console.WriteLine(RecursiveNumbers(1, N));
 //     return result;
 // }
 
-string RecursiveNumbers(int start, int end)  // c рекурсией
+// string RecursiveNumbers(int start, int end)  // c рекурсией
+// {
+//     if(start == end)
+//     { 
+//     return start.ToString();
+//     }
+//     return start + " " + RecursiveNumbers(start+1, end);
+// }
+                                                            //*** *** *** ***
+//Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N
+
+// Console.Clear();
+// Console.Write("Введите значение M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// // Numbers(N);
+// Console.WriteLine(RecursiveNumbers(M, N));
+
+// string RecursiveNumbers(int start, int end)  // c рекурсией
+// {
+//     if(start == end)
+//     { 
+//     return start.ToString();
+//     }
+//     return start + " " + RecursiveNumbers(start+1, end);
+// }
+                                                            //*** *** *** ***
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int SumNumberRecurs(int n)
 {
-    if(start == end)
-    { 
-    return start.ToString();
-    }
-    return start + " " + RecursiveNumbers(start+1, end);
+    if (n == 0) return 0; //условие окончания работы рекурсии
+    return n % 10 + SumNumberRecurs(n/10);
 }
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($" {number} -> {SumNumberRecurs(number)}");
