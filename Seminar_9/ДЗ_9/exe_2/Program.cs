e64ad0f33d60174db46f61fd74af95a28da4be42
Alena <gertmanea@mail.ru>
2022-07-22 19:10:10 +0300

    ДЗ9

--- Seminar_9/ДЗ_9/exe_2/Program.cs
+++ Seminar_9/ДЗ_9/exe_2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая 
+//найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumberRecurs(int m, int n)
+{
+    if(m == n || m == 0) return m;
+    else if(m < n)
+    {
+        return m + SumNumberRecurs(m + 1, n);
+    }
+    else 
+    {
+        return m + SumNumberRecurs(m - 1, n);
+    }
+}
+Console.Clear();
+Console.WriteLine("Введите стартовое число: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число: ");
+int end = Convert.ToInt32(Console.ReadLine());
+Console.Write(SumNumberRecurs(start, end));
