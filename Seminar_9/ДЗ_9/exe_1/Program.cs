2547f81228ab07e01668a0332dbd1cbab9a4e7c2
Alena <gertmanea@mail.ru>
2022-07-22 19:00:24 +0300

    ДЗ9

--- Seminar_9/ДЗ_9/exe_1/Program.cs
+++ Seminar_9/ДЗ_9/exe_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string RecursiveNumber (int m, int n)
+{
+    if(m == n) return m.ToString();
+    return m + " " + RecursiveNumber(m+1, n);
+}
+
+Console.WriteLine("Введите стартовое число: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число: ");
+int end = Convert.ToInt32(Console.ReadLine());
+Console.Write(RecursiveNumber(start, end));
