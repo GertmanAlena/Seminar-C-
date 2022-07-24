26eee865fbe8bfcb20255bc4031b6f816ce8fa33
Alena <gertmanea@mail.ru>
2022-07-24 11:46:32 +0300

    задачки

---
+++ String_Work/exe_Srting/Program.cs
@@ -0,0 +1,16 @@
+﻿// В заданной строке текста определите количество слов. 
+// Каждое слово отделено друг от друга пробелом.
+Console.WriteLine("press something");
+string line = Console.ReadLine();
+string probel = " ";
+int i, kolslov = 1, kolbukv = 0;
+for (i = 0; i != line.Length; i++)
+{
+   if (line[i] != probel[0]) 
+      kolbukv++;
+   else
+      kolslov++;
+}
+Console.WriteLine("your message is: " + line);
+Console.WriteLine("slova {0} bukvi {1}", kolslov, kolbukv);
+Console.ReadKey();
